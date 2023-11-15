ebe4459b0256004731afed0fdad1aa00d067fdfa
paperrronin <sateted@proton.me>
2023-11-15 22:34:00 +0200

    feat 404 page

--- src/app/authentication/authentication-routing.module.ts
+++ src/app/authentication/authentication-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Page404Component } from './page404/page404.component';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 const routes: Routes = [
@@ -15,7 +16,11 @@ const routes: Routes = [
   {
     path: 'signup',
     component: SignupComponent,
-  }
+  },
+  {
+    path: 'page404',
+    component: Page404Component,
+  },
   
 ];
 @NgModule({
